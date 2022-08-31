304d94754839f4c772775d290c7e1a12aeab6bc2
erhan11 <77719565+erhan11@users.noreply.github.com>
2022-08-31 18:03:26 +0300

    added seperator component

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
 import react from "react";
-import Chats from "./screens/Chats";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "react-navigation-stack";
+import { SafeAreaView, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>hello, homescreen</Text>
+    </SafeAreaView>
+  );
+};
+
+const Settings = () => {
+  return (
+    <SafeAreaView>
+      <Text>hi settings page</Text>
+    </SafeAreaView>
+  );
+};
 
 const App = () => {
-  return <Chats />;
+  return <NavigationContainer></NavigationContainer>;
 };
 
 export default App;
